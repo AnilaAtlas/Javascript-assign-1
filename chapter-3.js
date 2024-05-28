da969923b96c9e773a3b32313247b677325f7a45
AnilaAtlas <anila.khan3044@gmail.com>
2024-05-27 17:38:04 -0700

    first commit

---
+++ chapter-3.js
@@ -0,0 +1,27 @@
+
+// 1. Declare a variable called age & assign to it your age. Show
+// your age in an alert box.
+
+var age = 15;
+alert("I am"+ " " + age + " " + "year old");
+
+
+
+//2. Declare & initialize a variable to keep track of how many
+// times a visitor has visited a web page. Show his/her
+// number of visits on your web page. For example: “You
+// have visited this site N times”.
+
+var num = 8;
+alert("You have visited this site" + " " + num + " " + "times") ; 
+
+
+
+
+//  3. Declare a variable called birthYear & assign to it your
+// birth year. Show the following message in your browser:
+
+ document.write("My birth year is 1997");
+
+ 
+
---
+++ index.js
@@ -0,0 +1,85 @@
+//VARIABLES FOR STRINGS
+
+// 1. Declare a variable called username.
+
+var userName = ("User Name");
+console.log(userName);
+
+
+
+//2. Declare a variable called myName & assign to it a string
+// that represents your Full Name.
+
+var myName = ("Anila Atlas");
+console.log(myName);
+
+
+
+//  3. Write script to
+// a) Declare a JS variable, titled message.
+// b) Assign “Hello World” to variable message
+// c) Display the message in alert box.
+
+var message = ("Hello World!");
+alert(message);
+ 
+
+
+
+//4. Write a script to save student’s bio data in JS variables and
+// show the data in alert boxes.
+
+var studentName = ("Sohail khan");
+alert(studentName);
+
+var age = ("25");
+alert(age);
+
+var appliedCourses = ("Web and App development & Cloud Generative AI");
+alert(appliedCourses);
+
+
+
+
+
+//6. Declare a variable called email and assign to it a string that
+// represents your Email Address(e.g. example@example.com).
+// Show the blow mentioned message in an alert box.(Hint: use
+// string concatenation)
+
+var email = ("anila.khan3044@gmail.com");
+alert("My email address is @"  + email);
+
+
+
+
+
+// 7. Declare a variable called book & give it the value “A
+// smarter way to learn JavaScript”. Display the following
+// message in an alert box:
+
+var book = ("A smarter way to learn Javascript.");
+alert("I am trying to learn from the BOOK" +" " + book);
+
+
+
+
+// 8. Write a script to display this in browser through JS
+
+document.write("Yah! I can write HTML content through Javascript");
+
+
+
+// 9. Store following string in a variable and show in alert and
+// browser through JS
+// “▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”
+
+var end = ("▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬");
+alert (end);
+
+
+
+
+
+
+
